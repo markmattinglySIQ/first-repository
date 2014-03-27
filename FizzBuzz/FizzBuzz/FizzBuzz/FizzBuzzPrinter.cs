fef8614eb1e0021f4c128e7439b78e334a2b222c
markmattinglySIQ <mmattingly@solutionsiq.com>
2014-03-26 17:18:21 -0700

    Adding FizzBuzz solution to the repository

---
+++ FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzzPrinter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace FizzBuzz
+{ 
+    public class FizzBuzzPrinter
+    {
+        //public string   PrintNumber(int i)
+        public void PrintNumber(int i)
+        {
+            //StringBuilder returnValue = new StringBuilder();
+            bool fizzAndOrBuzz = false; 
+
+            if (i % 3 == 0)
+            {
+                //returnValue.Append("Fizz");
+                Console.Write("Fizz");
+                fizzAndOrBuzz = true;
+            }
+
+            if (i % 5 == 0)
+            {
+                //returnValue.Append("Buzz");
+                Console.Write("Buzz");
+                fizzAndOrBuzz = true;
+            }
+
+            if (!fizzAndOrBuzz)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
+            //return string.IsNullOrEmpty(returnValue.ToString()) ? i.ToString() : returnValue.ToString() ;
+        }
+    }
+}
